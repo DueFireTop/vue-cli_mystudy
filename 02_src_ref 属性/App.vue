d04d9c7b615585267c836f311ad3c8dd583c91d9
DueFireTop <67047199+DueFireTop@users.noreply.github.com>
2024-09-05 16:52:32 +0800

    Add files via upload

---
+++ 02_src_ref 属性/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="myh1"></h1>
+    <button ref="btn" @click="showDOM">点我输出上方的DOM元素</button>
+
+    <MySchool ref="mysch"></MySchool>
+  </div>
+</template>
+
+<script>
+    import MySchool from './components/School.vue'
+
+    export default {
+        name:'App',
+        components: { 
+                MySchool 
+            },
+        data() {
+            return {
+                msg: 'Hello Vue!'
+            }
+        },
+        methods: {
+            showDOM() {
+                console.log(this.$refs.myh1); // 获取到DOM元素 
+                console.log(this.$refs.btn); // 获取到DOM元素
+                console.log(this.$refs.mysch); // 获取到组件实例
+            }
+        }
+    }
+</script>
+
+<style>
+h1{
+    text-align: center;
+}
+</style>
---
+++ 02_src_ref 属性/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="demo-school">
+        <h2>学校名称：{{ schoolName }}</h2>
+        <h2>学校地址：{{ schoolAddress }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MySchool',
+    data() {
+        return {
+            schoolName: 'my school name',
+            schoolAddress: '北京昌平'
+        }
+    }
+}
+</script>
+
+<style>
+.demo-school{
+    background-color: #f00;
+    font-size: large;
+    color: blue;
+    text-align: center;
+}
+</style>
