22c2f07c486277004dd7f0f70158c88f7b9a5583
DueFireTop <67047199+DueFireTop@users.noreply.github.com>
2024-09-05 16:53:02 +0800

    Add files via upload

---
+++ 03_src_props配置/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MyStudent studentName="张三" studentGender="男"></MyStudent>
+  </div>
+</template>
+
+<script>
+    import MyStudent from './components/Student.vue'
+
+    export default {
+        name:'App',
+        components: { 
+            MyStudent 
+        },
+        
+    }
+</script>
+
+<style>
+h1{
+    text-align: center;
+}
+</style>
---
+++ 03_src_props配置/components/Student.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="demo-student">
+        <h2>学生姓名：{{ studentName }}</h2>
+        <h2>学生性别：{{ studentGender }}</h2>
+        <h2>学生年龄：{{ myAge }}</h2>
+        <button @click="updateStudentAge">修改年龄</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyStudent',
+    data() {
+        return {
+            myAge:this.studentAge
+        }
+    },
+    // props:['studentName', 'studentGender', 'studentAge'] // 接收父组件传递的属性（简单接收）
+
+    // 接收父组件传递的属性（复杂接收），对类型进行限制
+    // props:{
+    //     studentName: String,
+    //     studentGender: String,
+    //     studentAge: Number
+    // }
+
+    // 接收父组件传递的属性（复杂接收），对类型进行限制，对默认值进行设置
+    props:{
+        studentName: {
+            type: String, // 类型
+            required: true, // 是否为必传参数
+        },
+        studentGender: {
+            type: String,
+            default: 'default gender' // 默认值
+        },
+        studentAge: {
+            type: Number,
+            default: 99
+        }
+    },
+    methods: {
+        updateStudentAge(){
+            this.myAge = this.myAge+1
+        }
+    },
+}
+</script>
+
+<style>
+.demo-student{
+    background-color: #f00;
+    font-size: large;
+    color: blue;
+    text-align: center;
+}
+</style>
