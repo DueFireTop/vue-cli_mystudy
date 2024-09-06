4c2be2ef84d22b50b6bd1cd967e7b19cc4469ae8
DueFireTop <67047199+DueFireTop@users.noreply.github.com>
2024-09-06 10:04:24 +0800

    Add files via upload

---
+++ 06_src_scoped/components/School.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ schoolAddress }}</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MYSchool',
+    data() {
+        return {
+            name: '大声告诉萨夫是德国',
+            schoolAddress: '北京市'
+        }
+    },
+    
+}
+</script>
+
+<style scoped>
+.demo{
+    background-color: rgb(5, 21, 247);
+    font-size: large;
+    color: rgba(0, 0, 0, 0.397);
+    text-align: center;
+}
+</style>
---
+++ 06_src_scoped/components/Student.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+        <h2>学生姓名：{{ name }}</h2>
+        <h2>学生性别：{{ studentGender }}</h2>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'MyStudent',
+        data() {
+            return {
+                name: '张三', 
+                studentGender: '男'
+            }
+        },
+        
+    }
+</script>
+
+<!-- scoped: 表示当前样式只对当前组件有效 -->
+<style scoped>
+.demo{
+    background-color: #f00;
+    font-size: large;
+    color: blue;
+    text-align: center;
+}
+</style>
