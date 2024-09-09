c1d8de6ec8c63c58c45971292fcf9e6e4bd1eea4
DueFireTop <67047199+DueFireTop@users.noreply.github.com>
2024-09-09 09:35:42 +0800

    Add files via upload

---
+++ 07_src_todolist案例/components/TodoListContent.vue
@@ -0,0 +1,44 @@
+<template>
+    <ul class="todo-main">
+        <!-- 循环渲染 :todoItem="todoObj"表示传递给子组件-->
+        <TodoListContentItem 
+            v-for="todoObj in todoArr" 
+            :key="todoObj.id" 
+            :todoItem="todoObj" 
+            :checkTodo="checkTodo"
+            :deleteTodo="deleteTodo"
+        />
+    </ul>
+    
+</template>
+
+<script>
+import TodoListContentItem from './TodoListContentItem';
+
+export default {
+    name:'TodoListContent',
+    components:{
+        TodoListContentItem
+    },
+    props:['todoArr', 'checkTodo', 'deleteTodo']
+}
+</script>
+
+<style scoped>
+    /*main*/
+    .todo-main {
+        margin-left: 0px;
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        padding: 0px;
+    }
+
+    .todo-empty {
+        height: 40px;
+        line-height: 40px;
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        padding-left: 5px;
+        margin-top: 10px;
+    }
+</style>
