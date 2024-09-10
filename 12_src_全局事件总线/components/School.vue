adfdf800ccb71c343ac2c140fe66aaddeea402bd
DueFireTop <67047199+DueFireTop@users.noreply.github.com>
2024-09-10 14:54:02 +0800

    Add files via upload

---
+++ 12_src_全局事件总线/components/School.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="demo-school">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ schoolAddress }}</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MYSchool',
+    data() {
+        return {
+            name: '大声告诉萨夫是德国',
+            schoolAddress: '北京市'
+        }
+    },
+    mounted() {
+        // 监听sendStudentName事件
+        // 当该事件被触发并传递参数 name 时，会执行回调函数，打印出包含 name 的日志信息
+        // 这通常用于组件间通信，不直接父子关系的组件可通过 $bus 进行通信。
+        this.$bus.$on('sendStudentName', (name)=>{
+            console.log('我是school组件, 接收到了数据', name)
+        })
+    },
+    beforeDestroy() {
+        this.$bus.$off('sendStudentName')
+    },
+    
+}
+</script>
+
+<style scoped>
+.demo-school{
+    background-color: rgb(5, 21, 247);
+    font-size: large;
+    color: rgba(0, 0, 0, 0.397);
+    text-align: center;
+    padding: 5px;
+    margin-top: 10px; 
+}
+</style>
---
+++ 12_src_全局事件总线/components/Student.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="demo-student">
+        <h2>学生姓名：{{ name }}</h2>
+        <h2>学生性别：{{ studentGender }}</h2>
+        <button @click="sendStudentName2School">把学生名传递给MySchool组件</button>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'MyStudent',
+        data() {
+            return {
+                name: '张三', 
+                studentGender: '男'
+            }
+        },
+        methods: {
+            sendStudentName2School(){
+                // 触发自定义事件sendStudentName，通过事件传递当前组件的name属性值
+                this.$bus.$emit('sendStudentName', this.name)
+            }
+        },
+        
+        
+    }
+</script>
+
+<!-- scoped: 表示当前样式只对当前组件有效 -->
+<style scoped>
+.demo-student{
+    background-color: #f00;
+    font-size: large;
+    color: blue;
+    text-align: center;
+    padding: 5px;
+}
+</style>
