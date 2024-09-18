39eed458ebf7ecf03b33a1ce28a019c35976f704
DueFireTop <67047199+DueFireTop@users.noreply.github.com>
2024-09-18 10:57:15 +0800

    Add files via upload

---
+++ 32_src_命名路由/pages/HomeMessage.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="msg in messageList" :key="msg.id">
+                <!-- 跳转路由，携带query参数，to的字符串写法 -->
+                <!-- <router-link :to="`/home/message/detail?id=${msg.id}&title=${msg.title}`">{{msg.title}}</router-link>&nbsp;&nbsp; -->
+
+                <!-- 跳转路由，携带query参数，to的对象写法 -->
+                <router-link :to="{
+                    // path: '/home/message/detail', // 通过路由地址跳转
+                    name: 'homemessagedetail', // 通过路由名称跳转
+                    query: {
+                        id: msg.id,
+                        title: msg.title
+                    }
+                }">
+                    {{msg.title}}
+                </router-link>&nbsp;&nbsp;
+            </li>
+        </ul>
+        <hr>
+        <router-view></router-view>
+    </div>
+            
+</template>
+
+<script>
+import { name } from 'pubsub-js';
+
+export default {
+    name:'HomeMessage',
+    components: {},
+    data() {
+        return {
+            messageList:[
+                {
+                    id: 1,
+                    title: 'message001'
+                },
+                {
+                    id: 2,
+                    title: 'message002'
+                },
+                {
+                    id: 3,
+                    title: 'message003'
+                }
+                
+            ]
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 32_src_命名路由/router/index.js
@@ -0,0 +1,42 @@
+// 该文件专门用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+
+// 导入路由组件
+import MyAbout from '../pages/MyAbout.vue'
+import MyHome from '../pages/MyHome.vue'
+import HomeMessage from '../pages/HomeMessage.vue'
+import HomeNews from '../pages/HomeNews.vue'
+import HomeMessageDetail from '../pages/HomeMessageDetail.vue'
+
+// 创建并导出一个路由器
+export default new VueRouter({
+    routes:[
+        {
+            name:'myabout', // 路由名称
+            path:'/about',
+            component:MyAbout
+        },
+        {
+            path:'/home',
+            component:MyHome,
+            children:[
+                {
+                    path:'message',
+                    component:HomeMessage,
+                    children:[
+                        {
+                            name:'homemessagedetail',
+                            path:'detail',
+                            component:HomeMessageDetail
+                        }
+                    ]
+                },
+                {
+                    path:'news',
+                    component:HomeNews
+                }
+            ]
+        },
+        
+    ]
+})
